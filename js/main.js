73e10327c89587a7506414e4167483480bd23efe
Posttwo <posttwo@users.noreply.github.com>
2019-03-10 00:03:48 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var date = new Date(2019, 01, 11);
+var date = new Date(2019, 02, 10);
             var now = new Date();
             var diff = (now.getTime()/1000) - (date.getTime()/1000);
         
